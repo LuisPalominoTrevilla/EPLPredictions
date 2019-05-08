91a7dddb448ffd1bc8d2f284532b68d02172d3f9
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-05-08 00:36:41 -0500

    Removed App.vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,4 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
@@ -14,5 +13,5 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h('router-view')
 }).$mount('#app')
