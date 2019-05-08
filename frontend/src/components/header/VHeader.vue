6c112cc56094d3e6ae81425bb975b5ecfdc7ac44
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-05-08 02:07:11 -0500

    Finished header

---
+++ frontend/src/components/header/VHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <img class="epl-logo" src="@/assets/premier-league.png" alt="">
+        <team-listing></team-listing>
+        <v-menu></v-menu>
+    </div>
+</template>
+
+<script>
+import VMenu from './VMenu.vue';
+import TeamListing from './TeamListing.vue';
+
+export default {
+    components: {
+        VMenu,
+        TeamListing
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.epl-logo {
+    width: 7rem;
+    left: 0;
+    top: 0;
+    position: absolute;
+}
+</style>
---
+++ frontend/src/components/header/VMenu.vue
@@ -0,0 +1,49 @@
+<template>
+    <b-nav class="menu">
+        <b-nav-item>Premier League</b-nav-item>
+        <b-nav-item>Predictions</b-nav-item>
+        <b-nav-item>Stats</b-nav-item>
+    </b-nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.menu {
+    background-color: #38003c;
+    padding-left: 10%;
+    font-size: 1.1rem;
+
+    .nav-item {
+        position: relative;
+        .nav-link {
+            color: #ffffff;
+            text-decoration: none;
+            padding-top: 1rem;
+            padding-bottom: 1rem;
+
+            &:before {
+                content: "";
+                display: block;
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                height: 0;
+                z-index: -1;
+                transition: .2s;
+                background-color: #e90052;
+            }
+            
+            &:hover:before {
+                height: 4px;
+                z-index: 0;
+            }
+        }   
+    }
+}
+</style>
